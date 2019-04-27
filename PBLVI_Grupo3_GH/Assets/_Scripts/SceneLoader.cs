c1bb3b7c7c122a6c071ba6230b4c5f8b47da8d3e
Claudia Abate <claudia.abate98@gmail.com>
2019-04-28 01:43:16 +0200

    Menú principal + Controles + Créditos

--- PBLVI_Grupo3_GH/Assets/_Scripts/SceneLoader.cs
+++ PBLVI_Grupo3_GH/Assets/_Scripts/SceneLoader.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-	//Llamamos a funciones públicas con el botón derecho en el inspector del componente
-	[ContextMenu("LoadGame")]
-	
+    public GameObject UIControls;
+    public GameObject UICredits;
+
+    //Llamamos a funciones públicas con el botón derecho en el inspector del componente
+    [ContextMenu("LoadGame")]
+    private void Start()
+    {
+        UIControls.SetActive(false);
+        UICredits.SetActive(false);
+    }
+
     public void LoadGame()
 	{
 		//Carga la escena que esté en el índice 1 de File > Build Settings > Scenes
@@ -23,4 +31,16 @@ public class SceneLoader : MonoBehaviour
 		//Carga la escena que esté en el índice 0 de File > Build Settings > Scenes
 		SceneManager.LoadScene(0);
 	}
+
+    public void Controls()
+    {
+        //Muestra el panel de controles
+        UIControls.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        //Muestra el panel de créditos
+        UICredits.SetActive(true);
+    }
 }
