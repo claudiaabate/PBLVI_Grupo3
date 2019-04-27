e61e787aa58571f7b9d3c0d42b39f64d18baa6ad
Claudia Abate <claudia.abate98@gmail.com>
2019-04-27 21:12:17 +0200

    Estructura menú principal

---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	//Llamamos a funciones públicas con el botón derecho en el inspector del componente
+	[ContextMenu("LoadGame")]
+	
+    public void LoadGame()
+	{
+		//Carga la escena que esté en el índice 1 de File > Build Settings > Scenes
+		SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame()
+	{
+		//Cierra la aplicación (solo en Build)
+		Application.Quit();
+	}
+
+	protected void LoadMainMenu()
+	{
+		//Carga la escena que esté en el índice 0 de File > Build Settings > Scenes
+		SceneManager.LoadScene(0);
+	}
+}
