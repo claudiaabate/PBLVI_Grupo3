128f7086b337f76d172071b640466358c8ee2209
Claudia Abate <claudia.abate98@gmail.com>
2019-04-29 19:51:43 +0200

    Personaje y camara
    Provisionales.

---
+++ PBLVI_Grupo3_GH/Assets/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Arkanoid
+{
+    public class CharacterController : MonoBehaviour
+    {
+        public float speed = 2f; //la f al lado del 2 indica que es float
+        public UnityEngine.CharacterController charController;
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Mirar en project settings (Unity)
+            float horizontal = Input.GetAxis("Horizontal");
+            Debug.Log("horizontal: " + horizontal);
+
+            float vertical = Input.GetAxis("Vertical");
+            Debug.Log("vertical: " + horizontal);
+
+            Vector3 velocity = new Vector3(horizontal, 0, vertical);
+            Debug.Log("velocity: " + velocity);
+
+            Debug.Log("-------------------------------------------");
+
+            velocity = speed * velocity;
+
+            charController.SimpleMove(velocity);
+        }
+    }
+}
