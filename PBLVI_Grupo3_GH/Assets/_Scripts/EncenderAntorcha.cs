eb31d40e0cae223f54bf3f64f8201bdde05d3a80
TomasMoratoPerezPorro <tomasmpp@gmail.com>
2019-04-30 00:26:41 +0200

    Puzle ANtorchas

---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/EncenderAntorcha.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncenderAntorcha : MonoBehaviour
+{
+    
+    public bool on = false;
+    private Light antorcha;
+
+    void Start()
+    {
+        antorcha = GetComponent<Light>();
+    }
+
+    private void OnTriggerStay(Collider plyr)
+    {
+        if(plyr.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !on)
+        {
+            antorcha.enabled = !antorcha.enabled;
+            on = true;
+        }
+
+        else if (plyr.tag=="Player" && Input.GetKeyDown(KeyCode.E) && on)
+        {
+            antorcha.enabled = false;
+            on = false;
+        }
+    }
+
+
+}
---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/SolucionPuzleUno.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolucionPuzleUno : MonoBehaviour
+{
+    public EncenderAntorcha Vatra_01;
+    public EncenderAntorcha Vatra_02;
+    public EncenderAntorcha Vatra_03;
+    public EncenderAntorcha Vatra_04;
+    public EncenderAntorcha Vatra_05;
+    public EncenderAntorcha Vatra_06;
+    public EncenderAntorcha Vatra_07;
+    public EncenderAntorcha Vatra_08;
+    public EncenderAntorcha Vatra_09;
+    public EncenderAntorcha Vatra_10;
+    public EncenderAntorcha Vatra_11;
+    public EncenderAntorcha Vatra_12;
+
+    public Material Material_WIN;
+    public Material Material_LOSE;
+    private Renderer _renderer;
+
+    void Start()
+    {
+        _renderer = transform.GetComponent<Renderer>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            CheckTheResult();
+        }
+    }
+
+    void CheckTheResult()
+    {
+        if (Vatra_01.on == false && Vatra_02.on == true && Vatra_03.on == false && Vatra_04.on == true && Vatra_05.on == true && Vatra_06.on == false && Vatra_07.on == true && Vatra_08.on == false && Vatra_09.on == true && Vatra_10.on == true && Vatra_11.on == false && Vatra_12.on == true)
+        
+        {
+            Debug.Log("ES CORRECTE");
+            _renderer.material = Material_WIN;
+
+        }
+
+        else
+        {
+            Debug.Log("ES INCORRECTE");
+            _renderer.material = Material_LOSE;
+        }
+    }
+}
