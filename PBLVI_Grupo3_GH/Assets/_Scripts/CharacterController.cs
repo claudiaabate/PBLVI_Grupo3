c29707ad48179c0a1b3dba401be0352005e8cefc
ClaudiaCano <claudiacanolucena@outlook.com>
2019-05-02 13:30:42 +0200

    Arrastrat objetos
    Mecanica de arrastrar los objetos por la habitacion, falta el sprite de información

---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 2f;
+    public UnityEngine.CharacterController charController;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(horizontal, 0, vertical);
+
+        velocity = speed * velocity;
+
+        charController.SimpleMove(velocity);
+    }
+}
