dec4dba0154aeded136949c7d3c143109c7fe56f
Claudia Abate <claudia.abate98@gmail.com>
2019-05-03 23:22:29 +0200

    Sala de las puertas programada

--- PBLVI_Grupo3_GH/Assets/_Scripts/Doors.cs
+++ PBLVI_Grupo3_GH/Assets/_Scripts/Doors.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Doors : MonoBehaviour
 {
     public GameObject textinfo;
-    public GameObject ra;
-    public GameObject horus;
-    public GameObject bastet;
+    public GameObject panel;
 
     public bool enter = true;
     public bool stay = true;
@@ -15,61 +13,29 @@ public class Doors : MonoBehaviour
     public float moveSpeed;
 
     private Animator _animator;
-    //public float OpenPosition;
-    //public float ClosePosition;
-    //public float Duration = 1;
 
-    // Start is called before the first frame update
+    public static bool GameIsPaused = false;
+
     void Start()
     {
         textinfo.gameObject.SetActive(false);
-        ra.gameObject.SetActive(false);
-        horus.gameObject.SetActive(false);
-        bastet.gameObject.SetActive(false);
+        panel.gameObject.SetActive(false);
 
         _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            ra.gameObject.SetActive(true);
-            textinfo.gameObject.SetActive(false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            horus.gameObject.SetActive(true);
-            textinfo.gameObject.SetActive(false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            bastet.gameObject.SetActive(true);
-            textinfo.gameObject.SetActive(false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            OpenDoor();
-            textinfo.gameObject.SetActive(false);
+            if (GameIsPaused)
+            {
+                textinfo.gameObject.SetActive(true);
+                panel.gameObject.SetActive(false);
+                Resume();
+            }
         }
     }
-
-    /*public void OnMouseDown()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            camera_general.gameObject.SetActive(false);
-            camera_detail.gameObject.SetActive(true);
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            camera_general.gameObject.SetActive(true);
-            camera_detail.gameObject.SetActive(false);
-        }
-    }*/
 
     private void OnTriggerEnter(Collider other)
     {
@@ -79,16 +45,18 @@ public class Doors : MonoBehaviour
         }
     }
 
-    // stayCount allows the OnTriggerStay to be displayed less often
-    // than it actually occurs.
-    //private float stayCount = 0.0f;
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            ra.gameObject.SetActive(false);
-            horus.gameObject.SetActive(false);
-            bastet.gameObject.SetActive(false);
+            panel.gameObject.SetActive(true);
+            textinfo.gameObject.SetActive(false);
+            Pause();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            OpenDoor();
             textinfo.gameObject.SetActive(false);
         }
     }
@@ -101,30 +69,20 @@ public class Doors : MonoBehaviour
         }
     }
 
-
     public void OpenDoor()
     {
-        //StartCoroutine(OpenSmoothly());
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 1000, 0), 2 * Time.deltaTime);
-        //Quaternion rotation = Quaternion.Euler(0, 30, 0);
         _animator.SetBool("ShouldOpen", true);
     }
 
-    /*IEnumerator OpenSmoothly()
+    public void Resume()
     {
-        float x = transform.rotation.x;
-        float y = OpenPosition;
-        float z = transform.rotation.z;
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
 
-        float t = 0;
-        while (t < Duration)
-        {
-            t += Time.deltaTime;
-
-            y = Mathf.Lerp(ClosePosition, OpenPosition, t / Duration);
-            //transform.position = new Vector3(x, y, z);
-            Quaternion rotation = Quaternion.Euler(x, y, z);
-            yield return null;
-        }
-    }*/
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
 }
