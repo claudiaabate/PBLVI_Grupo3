a34ea7ef4c1b870b76c71b96f2bc7e28d0c8e1de
Mònica Huertas Cotonat <monica.huertas.citm@gmail.com>
2019-05-06 11:34:41 +0200

    Inventario puesto + recogida de monedas. Falta perfeccionar contador

---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/Inventario.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventario : MonoBehaviour
+{
+    public GameObject _inventario;
+    public GameObject _monedaRa;
+    public GameObject _monedaHorus;
+    public GameObject _monedaBastet;
+
+    public GameObject _text;
+
+    public Text _textContadorRa;
+    public int _monedasRa = 0;
+
+
+
+    void Start()
+    {
+        _inventario.SetActive(false);
+        _monedaRa.SetActive(false);
+        _monedaHorus.SetActive(false);
+        _monedaBastet.SetActive(false);
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            _inventario.SetActive(!_inventario.activeSelf);
+            _text.SetActive(false)
+;       }
+    }
+
+
+}
---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/Monedas.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monedas : Inventario
+{
+    public GameObject _Ra;
+    public GameObject _Horus;
+    public GameObject _Bastet;
+
+    
+
+    void Start()
+    {
+        _text.SetActive(false);
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        _text.SetActive(true);
+        _monedaRa.SetActive(true);
+
+        if (_Ra)
+        {
+            _monedasRa = _monedasRa + 1;
+            _textContadorRa.text = _monedasRa.ToString();
+        }
+
+        _Ra.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+}
