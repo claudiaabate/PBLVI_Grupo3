f01e0f99d6c3fbf4317e8450f0b57bf05f4e9645
Mònica Huertas Cotonat <monica.huertas.citm@gmail.com>
2019-05-11 20:26:43 +0200

    progresos puzzle 1 bastet

--- PBLVI_Grupo3_GH/Assets/_Scripts/Estatuas.cs
+++ PBLVI_Grupo3_GH/Assets/_Scripts/Estatuas.cs
@@ -7,18 +7,29 @@ public class Estatuas : MonoBehaviour
     public GameObject _estatuaSin;
     public GameObject _estatua;
 
+    private Animator _animator1;
+    private Animator _animator2;
+
+    public GameObject pared1;
+    public GameObject pared2;
+
     public GameObject _textEstatua;
 
     public bool enter = true;
     public bool exit = true;
 
-
-
     void Start()
     {
         _estatua.SetActive(false);
         _textEstatua.SetActive(false);
+
+        pared1.SetActive(false);
+        pared2.SetActive(false);
+
+        _animator1 = pared1.GetComponent<Animator>();
+        _animator2 = pared2.GetComponent<Animator>();
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -28,12 +39,20 @@ public class Estatuas : MonoBehaviour
         }
     }
 
+
     private void OnTriggerExit(Collider other)
     {
         if (exit)
         {
             _textEstatua.SetActive(false);
+
+            pared1.SetActive(true);
+            pared2.SetActive(true);
+
+            _animator1.SetBool("Cierre1", true);
+            _animator2.SetBool("Cierre2", true);
         }
+
     }
 
 }
