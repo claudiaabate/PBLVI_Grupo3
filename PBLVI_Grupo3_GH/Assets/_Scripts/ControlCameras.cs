379acad835c2498c5510a9f01b4b532de4fa9f9d
ClaudiaCano <claudiacanolucena@outlook.com>
2019-05-11 23:35:32 +0200

    Control de camaras
    Nueva camara en la habitacion de Orus

---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/ControlCameras.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ControlCameras : MonoBehaviour
+{
+    public GameObject camera_general;
+    public GameObject camera_orus;
+
+    void Start()
+    {
+        camera_general.gameObject.SetActive(true);
+        camera_orus.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            camera_general.gameObject.SetActive(false);
+            camera_orus.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            camera_general.gameObject.SetActive(true);
+            camera_orus.gameObject.SetActive(false);
+        }
+    }
+}
