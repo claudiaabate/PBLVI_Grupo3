11a892b2f9a8f06e2f85e6466fca723dc821794e
ClaudiaCano <claudiacanolucena@outlook.com>
2019-05-13 17:43:03 +0200

    Nueva cámara + mejora del puzzle 2
    cambio el tipo de camara y integracion de scripts y prefabs para el puzzle 2

---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/ControlPersonaje.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class ControlPersonaje : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private Rigidbody rb;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+    private Animator animator = null;
+    public bool playerInteractuando = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float ejeH = Input.GetAxis("Horizontal");
+        float ejeV = Input.GetAxis("Vertical");
+
+        moveInput = new Vector3(ejeH, 0f, ejeV);
+
+        Vector3 cameraForward = mainCamera.transform.forward;
+        cameraForward.y = 0;
+
+        Quaternion cameraRelativeRotation = Quaternion.FromToRotation(Vector3.forward, cameraForward);
+        Vector3 lookToward = cameraRelativeRotation * moveInput;
+
+        if (moveInput.sqrMagnitude > 0)
+        {
+            Ray lookRay = new Ray(transform.position, lookToward);
+            transform.LookAt(lookRay.GetPoint(1));
+        }
+
+        moveVelocity = transform.forward * speed * moveInput.sqrMagnitude;
+
+        Animating();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+    }
+
+    void Animating()
+    {
+        animator.SetFloat("blendSpeed", rb.velocity.magnitude);
+    }
+}
---
+++ PBLVI_Grupo3_GH/Assets/_Scripts/RotacionCamara.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotacionCamara : MonoBehaviour
+{
+
+    public float speedH = 2.0f;
+    public float speedV = 0.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    float m_FieldOfView;
+
+    void Start()
+    {
+        m_FieldOfView = 60.0f;
+        Camera.main.fieldOfView = m_FieldOfView;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+        }
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (Camera.main.fieldOfView > 60)
+            {
+                Camera.main.fieldOfView = Camera.main.fieldOfView - 5;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (Camera.main.fieldOfView < 90)
+            {
+                Camera.main.fieldOfView = Camera.main.fieldOfView + 5;
+            }
+        }
+    }
+}
