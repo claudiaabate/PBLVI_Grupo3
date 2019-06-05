96d4337293d2010141da88631f04df615544586f
Claudia Abate <claudia.abate98@gmail.com>
2019-06-05 16:14:58 +0200

    Iluminación Bastet + columnas + avances puzzle Ra

--- PBLVI_Grupo3_GH/Assets/_Scripts/EncenderAntorcha.cs
+++ PBLVI_Grupo3_GH/Assets/_Scripts/EncenderAntorcha.cs
@@ -9,15 +9,30 @@ public class EncenderAntorcha : MonoBehaviour
     public bool incorrecta = false;
     public GameObject ui;
 
-    private Light antorcha;
-    public ApagarTodasLuces ControladoraLlums;
+    //private Light antorcha;
+    private Renderer fuego;
 
+    public ApagarTodasLuces ControladoraLlums;
 
     void Start()
     {
-        antorcha = GetComponent<Light>();
+        //antorcha = GetComponent<Light>();
+
+        fuego = GetComponent<MeshRenderer>();
+
+        Renderer[] rs = GetComponentsInChildren<MeshRenderer>();
+        foreach (Renderer r in rs)
+            r.enabled = false;
+
+        Light[] ls = GetComponentsInChildren<Light>();
+        foreach (Light l in ls)
+            l.enabled = false;
+
         on = false;
-        antorcha.enabled = false;
+        //antorcha.enabled = false;
+
+        fuego.enabled = false;
+
         //Debug.Log("EL objeto" + gameObject.name + "Tiene la booleana en: " + incorrecta);
     }
 
@@ -64,13 +79,36 @@ public class EncenderAntorcha : MonoBehaviour
 
     void TurnOn()
     {
-        antorcha.enabled = !antorcha.enabled;
+        //antorcha.enabled = !antorcha.enabled;
+
+        fuego.enabled = !fuego.enabled;
+
+        Renderer[] rs = GetComponentsInChildren<MeshRenderer>();
+        foreach (Renderer r in rs)
+            r.enabled = !r.enabled;
+
+        Light[] ls = GetComponentsInChildren<Light>();
+        foreach (Light l in ls)
+            l.enabled = !l.enabled;
+
+
         on = true;
     }
 
     void TurnOff()
     {
-        antorcha.enabled = false;
+        //antorcha.enabled = false;
+
+        fuego.enabled = false;
+
+        Renderer[] rs = GetComponentsInChildren<MeshRenderer>();
+        foreach (Renderer r in rs)
+            r.enabled = false;
+
+        Light[] ls = GetComponentsInChildren<Light>();
+        foreach (Light l in ls)
+            l.enabled = false;
+
         on = false;
     }
 
